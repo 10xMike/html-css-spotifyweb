83ecad757f1743ccdfb0216b0c2203e0a47a2aed
10xMike <michelegec@gmail.com>
2021-05-06 01:38:03 +0200

    Inserite barre del tempo e del volume

--- spotify/spotify/js/main.js
+++ spotify/spotify/js/main.js
@@ -1,6 +1,6 @@
-// Use document.writeln
+/* // Use document.writeln
     document.writeln('Bella rega');
 // Use alert
     alert('Ao');
 // Use getElementById
-    document.getElementById('content').innerHTML = "Daje";
+    document.getElementById('content').innerHTML = "Daje"; */
